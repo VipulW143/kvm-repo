9596b1dcaf531551299c7ed6a09cc0e4712da4bf
karthik vushakola <karthik@pragiti.com>
2017-04-03 20:44:24 +0530

    Initial Commit

---
+++ bin/ext-kvm/kvm/kvmfacades/src/com/hybris/kvm/facades/constants/KvmFacadesConstants.java
@@ -0,0 +1,25 @@
+/*
+ * [y] hybris Platform
+ *
+ * Copyright (c) 2017 SAP SE or an SAP affiliate company.  All rights reserved.
+ *
+ * This software is the confidential and proprietary information of SAP
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with SAP.
+ */
+package com.hybris.kvm.facades.constants;
+
+/**
+ * Global class for all KvmFacades constants.
+ */
+@SuppressWarnings("PMD")
+public class KvmFacadesConstants extends GeneratedKvmFacadesConstants
+{
+	public static final String EXTENSIONNAME = "kvmfacades";
+
+	private KvmFacadesConstants()
+	{
+		//empty
+	}
+}
